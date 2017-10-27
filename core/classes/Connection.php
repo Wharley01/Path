4b597e3e8031fac7a014e9e5d3dd36dabbf8f51f
Wharley01 <hackerslord96@gmail.com>
2017-10-27 15:31:30 +0100

    Create connection class created

---
+++ core/classes/Connection.php
@@ -0,0 +1,19 @@
+<?php
+namespace Connection;
+class Connect{
+  private $HOST = "localhost";
+  private $USER = "";
+  private $PASSWORD = "";
+  private $DATABASE = "";
+  public $connection = null;
+  function __construct(){
+    try {
+$this->connection = new PDO("mysql:host={$this->HOST};dbname={$this->DATABASE}", $this->USER, $this->PASSWORD);
+    } catch (Exception $e) {
+      die($e->getMessage());
+    }
+
+
+  }
+}
+?>
