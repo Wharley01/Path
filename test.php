73dade9f4f5449df6ede2ca45f1baf536d0fa183
Wharley01 <hackerslord96@gmail.com>
2017-10-29 11:00:54 +0100

    more case studies added

--- test.php
+++ test.php
@@ -6,9 +6,10 @@ use Data\Database;
 $db = new Database(new Mysql());
 $age = 12;
 $db->
-select("*")
+select("Name")
     ->from("test_table")
-    ->where(['id' => 1]);
+    ->where(['id' => 1])
+    ->where('id = 2');
 
 print_r($db->getResult(false));
 
