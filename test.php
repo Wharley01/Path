b98c0930d82ebdd8267caf57ca417d76da758c6f
Wharley01 <hackerslord96@gmail.com>
2017-10-29 06:41:27 +0100

    indent example test

--- test.php
+++ test.php
@@ -5,7 +5,15 @@ use Data\Database;
 
 $db = new Database(new Mysql());
 $age = 12;
-$db->select("name")->from("employees")->where('name > wale')->where(['school' => 'test'])->where(['do' => 'sleep'])->where(['address' => 'ILOBU','food' => 'eba'])->or_where('status = alive')->or_where(['school'=>'mahmud']);
+$db->
+select("name")
+    ->from("employees")
+    ->where('name > wale')
+    ->or_where(['school' => 'test'])
+    ->where(['do' => 'sleep'])
+    ->where(['address' => 'ILOBU','food' => 'eba'])
+    ->or_where('status = alive')
+    ->or_where(['school'=>'mahmud']);
 
 print_r($db->query_data);
 
