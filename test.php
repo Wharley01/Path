80d7d44a6adbfd3dc5c8552a725df00379284bd6
Wharley01 <hackerslord96@gmail.com>
2017-11-18 15:22:59 +0100

    More test cases added

--- test.php
+++ test.php
@@ -4,25 +4,38 @@ use Connection\Mysql;
 use Data\Database;
 
 $db = new Database(new Mysql());
-
-$select = $db->Select('Name')
-             ->From('test_table')
-             ->Where('Name')
-             ->Like('Ade')
-             ->orWhere('Name')
-             ->Like('Sul')
-             ->Get();
-
-print_r($select);
-
-//try{
-//    $db->Exe();
-//}catch (Exception $exception){
-//    echo $exception->getMessage();
-//}
+//Select Data
+$select = $db->Select('Name')->From('test_table')->Where(['ID'=>1])->Get();
+print_r($select);//Print result
 
 
+//Update
+ $db->Update('test_table')
+     ->Set(['Name'=>'Sulaimen'])
+     ->Where('Name')
+     ->Like('wale')
+     ->Where('ID = 3');
+//Try executing the query
+ try{
+     $db->Exe();//OR ->Execute();
+ }catch (Exception $e){
+     echo $e->getMessage();
+ }
 
+//Insert
+$db->Insert(['Name'=>'Sulaimen','Age' => '34'])
+    ->Into('test_table');
+//Try executing the query
+try{
+    $db->Exe();//OR ->Execute();
+}catch (Exception $e){
+    echo $e->getMessage();
+}
+
+//DELETE
+$db->deleteFrom('test_table')
+    ->Where('Name = wale')
+    ->Execute();
 
 
 ?>
