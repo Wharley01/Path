2d6e84832ce4e9e9020e62c0459b6e73eb4520cd
Wharley01 <hackerslord96@gmail.com>
2018-10-20 14:08:15 +0100

    Updated function, better error handling

--- core/classes/Database.php
+++ core/classes/Database.php
@@ -2,8 +2,11 @@
 namespace Data;
 use Connection\DB;
 class Database{
-    private $db_con;
+    public $db_con;
     public $query_data = array();
+    public $last_id;
+    public $exe;
+
     public function __construct(DB $connection)
     {
         $this->db_con = $connection->__construct();
@@ -12,6 +15,25 @@ class Database{
     /**
      * @param $columns
      */
+    public function Query($query,$type = 'fetch'){
+//        echo $query;
+//        var_dump($this->query_data['bind_data']);
+        $exe = $this->db_con->prepare($query);
+        if(@$this->query_data['bind_data']){
+            $exe->execute(@$this->query_data['bind_data']);
+        }else{
+            $exe->execute();
+        }
+        $this->exe = $exe;
+        if($type == 'fetch'){
+            return $exe->fetchAll(\PDO::FETCH_ASSOC);
+        }else{
+            return $this;
+        }
+    }
+    public function bind_data(Array $data){
+        $this->query_data["bind_data"] = $data;
+    }
     public function Select($columns){
         $this->query_data = [];
         $string = "";
@@ -48,19 +70,19 @@ class Database{
      * @param array $condition
      * @return $this
      */
-public function Where($conditions){
+    public function Where($conditions){
         if(func_num_args() < 1){
             return $this;
         }
-       if(is_array($conditions)){
+        if(is_array($conditions)){
             $string = "";
 
 
-                foreach($conditions as $condition => $value){
-                        $string .= " {$condition} = ? AND ";
-                        $this->query_data['bind_data'][] = $value;
-                }
-                $string = preg_replace("/(AND)\s*$/","",$string);
+            foreach($conditions as $condition => $value){
+                $string .= " {$condition} = ? AND ";
+                $this->query_data['bind_data'][] = $value;
+            }
+            $string = preg_replace("/(AND)\s*$/","",$string);
             if(!isset($this->query_data['where'])){
                 @$this->query_data['where'] .= $string;
             }else{
@@ -103,7 +125,7 @@ public function Where($conditions){
             }
 //            $string = preg_replace("/(AND)\s*$/","",$string);
 
-                $this->query_data['where'] .= $string;
+            $this->query_data['where'] .= $string;
         }else if(preg_match('/^[_\w]*$/',$conditions)){
             $where = $this->query_data['where'];
             $this->query_data['where'] = "{$where} OR {$conditions}";
@@ -118,33 +140,33 @@ public function Where($conditions){
             }
             $string = preg_replace("/(OR)\s*$/","",$string);
 
-                $this->query_data['where'] .= $string;
+            $this->query_data['where'] .= $string;
 
         }else{
             throw new \Exception("Invalid value for 'orWhere' method");
         }
         return $this;
     }
-public function Like($compare){
-    $where = $this->query_data['where'];
-    $this->query_data['where'] = "{$where} LIKE ?";
-    $this->query_data['bind_data'][] = "%{$compare}%";
-    return $this;
-}
+    public function Like($compare){
+        $where = $this->query_data['where'];
+        $this->query_data['where'] = "{$where} LIKE ?";
+        $this->query_data['bind_data'][] = "%{$compare}%";
+        return $this;
+    }
     public function notLike($compare){
         $where = $this->query_data['where'];
         $this->query_data['where'] = "{$where} NOT LIKE ?";
         $this->query_data['bind_data'][] = "%{$compare}%";
         return $this;
     }
-public function Between($start,$stop){
+    public function Between($start,$stop){
         $where = $this->query_data['where'];
         $this->query_data['where'] = "{$where} BETWEEN ? AND ?";
         $this->query_data['bind_data'][] = $start;
         $this->query_data['bind_data'][] = $stop;
         return $this;
     }
-public function notBetween($start,$stop){
+    public function notBetween($start,$stop){
         $where = $this->query_data['where'];
         $this->query_data['where'] = "{$where} NOT BETWEEN ? AND ?";
         $this->query_data['bind_data'][] = $start;
@@ -152,183 +174,191 @@ public function notBetween($start,$stop){
 
         return $this;
     }
-public function Update($table){
-    $this->query_data = [];
-    if(func_num_args() < 1 || is_null($table) || empty($table)){
-        throw new \Exception("Specify table to update");
-    }
-    $this->query_data['table'] = $table;
-    $this->query_data['command'] = "UPDATE";
-
-    return $this;
-}
-
-
-
-public function Set($values){
-    if(func_num_args() < 1 || is_null($values) || empty($values)){
-        throw new \Exception('Set value to update');
-    }
-
-    if(is_array($values)){
-        $string = "";
-
-
-        foreach($values as $column => $value){
-            if(@$this->query_data['update_data']){
-                $string .= ",{$column} = ?,";
-                $this->query_data['bind_data'][] = $value;
-            }else{
-                $string .= "{$column} = ?,";
-                $this->query_data['bind_data'][] = $value;
-            }
-
+    public function Update($table){
+        $this->query_data = [];
+        if(func_num_args() < 1 || is_null($table) || empty($table)){
+            throw new \Exception("Specify table to update");
         }
+        $this->query_data['table'] = $table;
+        $this->query_data['command'] = "UPDATE";
+
+        return $this;
+    }
+
+
+
+    public function Set($values){
+        if(func_num_args() < 1 || is_null($values) || empty($values)){
+            throw new \Exception('Set value to update');
+        }
+
+        if(is_array($values)){
+            $string = "";
+
+
+            foreach($values as $column => $value){
+                if(@$this->query_data['update_data']){
+                    $string .= ",{$column} = ?,";
+                    $this->query_data['bind_data'][] = $value;
+                }else{
+                    $string .= "{$column} = ?,";
+                    $this->query_data['bind_data'][] = $value;
+                }
+
+            }
             $string = preg_replace("/,\s*$/","",$string);//remove trailing commar
 
-        @$this->query_data['update_data'] .= $string;
+            @$this->query_data['update_data'] .= $string;
 
-    }else if(preg_match("/\w+\s*[><=!]\s*\w+/",$values)){
+        }else if(preg_match("/\w+\s*[><=!]\s*\w+/",$values)){
 
-        $split = explode(",",$values);
-        $string = "";
-        foreach ($split as $val){
-            $match = preg_match("/(\w+)\s*([><=!])\s*(\w+)/",$val,$matches);
-            if(!empty($this->query_data['update_data'])){
-                $string .= ",{$matches[1]} = ?,";
-                $this->query_data['bind_data'][] = $matches[3];
-            }else{
-                $string .= "{$matches[1]} = ?,";
-                $this->query_data['bind_data'][] = $matches[3];
+            $split = explode(",",$values);
+            $string = "";
+            foreach ($split as $val){
+                $match = preg_match("/(\w+)\s*([><=!])\s*(\w+)/",$val,$matches);
+                if(!empty($this->query_data['update_data'])){
+                    $string .= ",{$matches[1]} = ?,";
+                    $this->query_data['bind_data'][] = $matches[3];
+                }else{
+                    $string .= "{$matches[1]} = ?,";
+                    $this->query_data['bind_data'][] = $matches[3];
+                }
+
             }
+            $string = preg_replace("/,\s*$/","",$string);
+
+            @$this->query_data['update_data'] .= $string;
 
         }
-        $string = preg_replace("/,\s*$/","",$string);
-
-        @$this->query_data['update_data'] .= $string;
-
+        return $this;
     }
-    return $this;
-}
-public function Insert($data){
-    $this->query_data = [];
-    if(func_num_args() < 1 || is_null($data) || empty($data)){
-        throw new \Exception('Set value to update');
-    }
+    public function Insert($data){
+        $this->query_data = [];
+        if(func_num_args() < 1 || is_null($data) || empty($data)){
+            throw new \Exception('Set value to update');
+        }
 
-    if(is_array($data)){
-        $string = "";
-        foreach($data as $column => $value){
-            if(@$this->query_data['update_data']){
-                $string .= ",{$column} = ?,";
-                $this->query_data['bind_data'][] = $value;
-            }else{
-                $string .= "{$column} = ?,";
-                $this->query_data['bind_data'][] = $value;
+        if(is_array($data)){
+            $string = "";
+            foreach($data as $column => $value){
+                if(@$this->query_data['update_data']){
+                    $string .= ",{$column} = ?,";
+                    $this->query_data['bind_data'][] = $value;
+                }else{
+                    $string .= "{$column} = ?,";
+                    $this->query_data['bind_data'][] = $value;
+                }
+
             }
+            $string = preg_replace("/,\s*$/","",$string);//remove trailing commar
+
+            @$this->query_data['update_data'] .= $string;
+
+        }else if(preg_match("/\w+\s*[><=!]\s*\w+/",$data)){
+
+            $split = explode(",",$data);
+            $string = "";
+            foreach ($split as $val){
+                $match = preg_match("/(\w+)\s*([><=!])\s*(\w+)/",$val,$matches);
+                if(!empty($this->query_data['update_data'])){
+                    $string .= ",{$matches[1]} = ?,";
+                    $this->query_data['bind_data'][] = $matches[3];
+                }else{
+                    $string .= "{$matches[1]} = ?,";
+                    $this->query_data['bind_data'][] = $matches[3];
+                }
+
+            }
+            $string = preg_replace("/,\s*$/","",$string);
+
+            @$this->query_data['update_data'] .= $string;
 
         }
-        $string = preg_replace("/,\s*$/","",$string);//remove trailing commar
-
-        @$this->query_data['update_data'] .= $string;
-
-    }else if(preg_match("/\w+\s*[><=!]\s*\w+/",$data)){
-
-        $split = explode(",",$data);
-        $string = "";
-        foreach ($split as $val){
-            $match = preg_match("/(\w+)\s*([><=!])\s*(\w+)/",$val,$matches);
-            if(!empty($this->query_data['update_data'])){
-                $string .= ",{$matches[1]} = ?,";
-                $this->query_data['bind_data'][] = $matches[3];
-            }else{
-                $string .= "{$matches[1]} = ?,";
-                $this->query_data['bind_data'][] = $matches[3];
-            }
-
+        return $this;
+    }
+    public function removeTrailingComma($str){
+        $str = preg_replace("/,$/","",$str);
+        return $str;
+    }
+    public function Into($table){
+        if(func_num_args() < 1 || is_null($table) || empty($table)){
+            throw new \Exception("Specify table to update");
         }
-        $string = preg_replace("/,\s*$/","",$string);
+        $this->query_data['table'] = $table;
+        $this->query_data['command'] = "INSERT INTO";
+        return $this;
+    }
+    public function deleteFrom($table){
+        if(func_num_args() < 1 || is_null($table) || empty($table)){
+            throw new \Exception("Specify table to delete from");
+        }
+        $this->query_data['table'] = $table;
+        $this->query_data['command'] = "DELETE FROM ";
+        return $this;
+    }
+    public function Exe(){
+        $query = "{$this->query_data['command']} {$this->query_data['table']}";
 
-        @$this->query_data['update_data'] .= $string;
-
-    }
-    return $this;
-}
-public function removeTrailingComma($str){
-    $str = preg_replace("/,$/","",$str);
-    return $str;
-}
-public function Into($table){
-    if(func_num_args() < 1 || is_null($table) || empty($table)){
-        throw new \Exception("Specify table to update");
-    }
-    $this->query_data['table'] = $table;
-    $this->query_data['command'] = "INSERT INTO";
-    return $this;
-}
-public function deleteFrom($table){
-    if(func_num_args() < 1 || is_null($table) || empty($table)){
-        throw new \Exception("Specify table to delete from");
-    }
-    $this->query_data['table'] = $table;
-    $this->query_data['command'] = "DELETE FROM ";
-    return $this;
-}
-public function Exe(){
-    $query = "{$this->query_data['command']} {$this->query_data['table']}";
-
-    if(@$this->query_data['update_data']){
-        $query .= " SET {$this->query_data['update_data']}";
-    }
+        if(@$this->query_data['update_data']){
+            $query .= " SET {$this->query_data['update_data']}";
+        }
 
 
-    if(@$this->query_data['where']){
-        $query .= " WHERE {$this->query_data['where']}";
+        if(@$this->query_data['where']){
+            $query .= " WHERE {$this->query_data['where']}";
+        }
+//        echo $query.'<br><br>';
+        try{
+            $exe = $this->db_con->prepare($query);
+            $exe->execute(@$this->query_data['bind_data']);
+            $this->last_id = $this->db_con->lastInsertId();
+            //echo "Query successfully executed";
+        }catch (\PDOException $e){
+            throw new \Exception($e->getMessage());
+        }
+        unset($this->query_data);
+        return $this;
     }
-    //echo $query.'<br><br>';
-    try{
-        $exe = $this->db_con->prepare($query);
-        $exe->execute(@$this->query_data['bind_data']);
-        //echo "Query successfully executed";
-    }catch (\PDOException $e){
-        throw new \Exception($e->getMessage());
+    public function Execute(){
+        $this->Exe();
     }
-
-    return true;
-}
-public function Execute(){
-    $this->Exe();
-}
 
     /**
      * @return array
      */
-public function orderBy($sort){
-    if(is_array($sort)){
-        $str = "";
-        foreach ($sort as $key => $val){
-            $str .=" {$key} {$val},";
-        }
-        $str = $this->removeTrailingComma($str);
-        $this->query_data['sort'] = " ORDER BY ".$str;
-    }else{
-        $this->query_data['sort'] = " ORDER BY ".$sort;
-    }
-    return $this;
-}
 
-public function Limit(
-    $start = 0,
-    $stop = 10
-){
-    if(is_nan($start) OR is_nan($stop)){
-        throw new \Exception("Invalid start or stop argument in Limit method");
+    public function orderBy($sort) : Database{
+        if(is_array($sort)){
+            $str = "";
+            foreach ($sort as $key => $val){
+                $str .=" {$key} {$val},";
+            }
+            $str = $this->removeTrailingComma($str);
+            $this->query_data['sort'] = " ORDER BY ".$str;
+        }else{
+            $this->query_data['sort'] = " ORDER BY ".$sort;
+        }
+        return $this;
     }
-    $this->query_data['limit'] = " LIMIT {$start},{$stop}";
-    return $this;
-}
-public function Get($is_array = true){
+
+    public function Limit(
+        $start = 0,
+        $stop = 10
+    ){
+        if(is_nan($start) OR is_nan($stop)){
+            throw new \Exception("Invalid start or stop argument in Limit method");
+        }
+        $this->query_data['limit'] = " LIMIT {$start},{$stop}";
+        return $this;
+    }
+    public function Get($is_array = true){
         $query = "";
+        if(!$this->query_data['table'])
+            throw new \Exception("Specify Table name");
+        if(!$this->query_data['column'])
+            throw new \Exception("Specify Column");
+
+
         $query = "SELECT {$this->query_data['column']} FROM {$this->query_data['table']}";
         if(@$this->query_data['where']){
             $query .= " WHERE {$this->query_data['where']}";
@@ -342,7 +372,11 @@ public function Get($is_array = true){
         try{
 //echo $query;
             $exe = $this->db_con->prepare($query);
-            $exe->execute(@$this->query_data['bind_data']);
+            if(@$this->query_data['bind_data']){
+                $exe->execute(@$this->query_data['bind_data']);
+            }else{
+                $exe->execute();
+            }
 
             if($is_array){
                 return $exe->fetchAll(\PDO::FETCH_ASSOC);
@@ -353,8 +387,9 @@ public function Get($is_array = true){
 
 
         }catch (\PDOException $e){
-            echo $e->getMessage();
+            throw new \Exception($e->getMessage());
         }
+        unset($this->query_data);
         return $this;
     }
 
