1f549d24b5627a853462ae14eea7560eafd44e4b
Wharley01 <hackerslord96@gmail.com>
2018-10-28 05:16:14 +0100

    Added more test

--- index.php
+++ index.php
@@ -12,13 +12,20 @@ load_class(["User"],"controllers");
 $router = new Router(new Request());
 
 try{
+    $router->ExceptionCatch(function ($req,$err){
+        return(new Response())->json($err,500);
+    });
+
     $router->GET(
         ["path"       => "/api/user/@user_name/@user_id:int",
          "middleware" => Response::MiddleWare(
         Path\Http\MiddleWare\Auth::class,
-        (new Response())->json(["error" => "User ID not 300"],401))],function ($params) use ($router){
+        function ($request,$params){
+            return (new Response())->json(["error" => "User ID not 300 it is {$params->user_id}","code" => 600,"params" => $params],401);
+         })],
+        function ($params) use ($router){
         return (new Response())->json([
-            "greet" => "Hello world new json API"
+            "greet" => "Hello world new json API {$router->request->fetch('name')}"
         ])->addHeader([
             "Access-Control-Allow-Origin" => "*"
         ]);
@@ -27,16 +34,20 @@ try{
 
     /** @var Router $router */
     @$router->POST("/api/user/@user_name/@user_id:int",function ($params) use ($router){
-        return (new Response("Hello world"))->addHeader([
+        return (new Response())->json(["msg" => "This is profile page {$router->request->fetch('name')}"])->addHeader([
             "Access-Control-Allow-Origin" => "*"
         ]);
+    });
+
+    $router->GET(["path" => '/user/@user_id:int'],function ($params){
+       return((new Response())->json((Array)$params,200));
     });
 
     $router->GET(["path" => '/'],function ($params){
        return(new Response("This is home page"));
     });
 
-    $router->Fallback(function (){
+    $router->Error404(function (){
         return (new Response("Error 404",404))->addHeader([
             "Access-Control-Allow-Origin" => "*"
         ]);
@@ -44,7 +55,7 @@ try{
 
 
 }catch (\Path\RouterException $e){
-    echo $e->getMessage();
+    echo "There was an error: ". $e->getMessage();
 }
 
 
