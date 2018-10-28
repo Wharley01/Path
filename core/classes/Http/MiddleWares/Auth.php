4e1d10922ceb0a6ce3be00677bbf426ff92fc3c0
Wharley01 <hackerslord96@gmail.com>
2018-10-28 05:14:50 +0100

    Added Auth middle ware for test

--- core/classes/Http/MiddleWares/Auth.php
+++ core/classes/Http/MiddleWares/Auth.php
@@ -1,19 +1,25 @@
 <?php
 
 namespace Path\Http\MiddleWare;
-use Path\Controller;
+use Connection\Mysql;
+use Data\Database;
+use Path\Controller\User;
 use Path\Http\MiddleWare;
-
+load_class(['Connection','Database']);
 load_class("User","controllers");
 
-class Auth extends MiddleWare
+class Auth extends User implements MiddleWare
 {
     public function __construct()
     {
+        parent::__construct(new Database(new Mysql()));
         return true;
     }
     public function Control($request,$params){
-        return $params->user_id == 300;
+        return $this->Auth($params);
+    }
+    public function test(){
+
     }
 
 }
