ebaf4f4b24eaa222ca8bde890c22c9dc14762a47
Wharley01 <hackerslord96@gmail.com>
2018-11-04 17:02:24 +0100

    Controllers now work with Routes

---
+++ core/controllers/Songs.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP ENVY
+ * Date: 11/4/2018
+ * Time: 1:48 AM
+ */
+
+namespace Path\Controller;
+use Path\Controller;
+use Path\Http\Request;
+use Path\Http\Response;
+
+class Songs
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     */
+    public function Find(Request $request, Response $response){
+
+    }
+    public function Delete(Request $request, Response $response){
+        return $response->html("<b>Delete user</b>",201);
+    }
+
+}
--- core/controllers/User.php
+++ core/controllers/User.php
@@ -7,26 +7,36 @@
  */
 
 namespace Path\Controller;
-
-
+load_class("Controller","controllers");
 use Data\Database;
 use Path\Controller;
 use Path\Http\Request;
+use Path\Http\Response;
 
 
 class User implements Controller
 {
     private $db_connection;
 
-    public function __construct(Database $database)
+    public function __construct()
     {
-        $this->db_connection = $database;
+//        $this->db_connection = $database;
     }
-    public function Delete(Request $request){
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function Delete(Request $request, Response $response){
+            return $response->json(['user_id' => $request->params->user_id,"action" => "DELETE FROM CONTROLLER"]);
 
     }
-    public function Auth($params){
-        return $params->user_id == 300;
+    public function Find(Request $request,Response $response){
+        return $response->json(['result' => (array) $request->params],200);
+    }
+    public function Auth(Request $request,Response $response){
+        return true;
     }
 
 }
