8bd3e38fc64ff217cfb50955d7da6a50289568a3
Wharley01 <hackerslord96@gmail.com>
2018-11-08 02:03:30 +0100

    added is_same method to deep check response in test

---
+++ core/classes/Test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP ENVY
+ * Date: 11/5/2018
+ * Time: 2:50 AM
+ */
+
+namespace Path;
+
+
+class Test
+{
+    public function __construct()
+    {
+    }
+    public static function is_same($arr1,$arr2){
+        return trim(json_encode($arr1)) == trim(json_encode($arr2));
+    }
+
+}
