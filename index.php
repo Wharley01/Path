5784dc052dd0d7ddc1d6c816c659ab521912cb89
Wharley01 <hackerslord96@gmail.com>
2018-11-08 02:04:47 +0100

    More test cases added

--- index.php
+++ index.php
@@ -6,54 +6,39 @@ use Path\Http\Router;
 
 require_once "core/kernel.php";
 
-load_class(["Http/Router","Http/Request","Http/Response"]);
-load_class(["User"],"controllers");
+load_class(["Http/Router","Http/Response"]);
+//load_class(["User"],"controllers");
 
-$router = new Router(new Request());
 
 try{
+    $router = new Router();
+
     $router->ExceptionCatch(function ($req,$err){
         return(new Response())->json($err,500);
     });
 
-    $router->GET(
-        ["path"       => "/api/user/@user_name/@user_id:int",
-         "middleware" => Response::MiddleWare(
-        Path\Http\MiddleWare\Auth::class,
-        function ($request,$params){
-            return (new Response())->json(["error" => "User ID not 300 it is {$params->user_id}","code" => 600,"params" => $params],401);
-         })],
-        function ($params) use ($router){
-        return (new Response())->json([
-            "greet" => "Hello world new json API {$router->request->fetch('name')}"
-        ])->addHeader([
-            "Access-Control-Allow-Origin" => "*"
-        ]);
-    });
+   $router->group(["path" => "/api/@api_version:[1-3]"],function (Router $router){
+
+            $router->POST("/delete/user/@user_id/","User->Find");
+
+            $router->GET("/user/@user_name",function (Request $req,Response $res){
+//           Do any Thing here
+            });
+//This is using Controller
+       $router->POST("/search/@query","User->Find");
 
 
-    /** @var Router $router */
-    @$router->POST("/api/user/@user_name/@user_id:int",function ($params) use ($router){
-        return (new Response())->json(["msg" => "This is profile page {$router->request->fetch('name')}"])->addHeader([
-            "Access-Control-Allow-Origin" => "*"
-        ]);
-    });
-
-    $router->GET(["path" => '/user/@user_id:int'],function ($params){
-       return((new Response())->json((Array)$params,200));
-    });
-
-    $router->GET(["path" => '/'],function ($params){
-       return(new Response("This is home page"));
-    });
-
-    $router->Error404(function (){
+       $router->Error404(function (){
+           return (new Response("Error 404 for group",404))->addHeader([
+               "Access-Control-Allow-Origin" => "*"
+           ]);
+       });
+   });
+   $router->Error404(function (){
         return (new Response("Error 404",404))->addHeader([
             "Access-Control-Allow-Origin" => "*"
         ]);
     });
-
-
 }catch (\Path\RouterException $e){
     echo "There was an error: ". $e->getMessage();
 }
