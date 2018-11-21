dbc795bf23ae494a56a70054d2cae14b94203d93
Wharley01 <hackerslord96@gmail.com>
2018-11-21 18:52:41 +0100

    DB Connection details now from config.ini

--- core/classes/Database/Connection.php
+++ core/classes/Database/Connection.php
@@ -19,13 +19,18 @@ class Mysql implements DB
 
     public function __construct()
     {
+        $HOST           = config("DATABASE->host");
+        $USER           = config("DATABASE->user");
+        $NAME           = config("DATABASE->name");
+        $PASSWORD       = config("DATABASE->pass");
+
         try {
             $opt = [
                 \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 \PDO::ATTR_EMULATE_PREPARES   => false,
             ];
-            $this->connection = new \PDO("mysql:host={$this->HOST};dbname={$this->DATABASE}", $this->USER, $this->PASSWORD,$opt);
+            $this->connection = new \PDO("mysql:host={$HOST};dbname={$NAME}", $USER, $PASSWORD,$opt);
             $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
             die($e->getMessage());
