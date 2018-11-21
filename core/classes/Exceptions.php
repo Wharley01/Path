d3d01a1f21bb279d22baae7a5727843c4f4b805c
Wharley01 <hackerslord96@gmail.com>
2018-11-21 18:51:53 +0100

    Database exception Class Added

--- core/classes/Exceptions.php
+++ core/classes/Exceptions.php
@@ -35,3 +35,15 @@ class RouterException extends \Exception{
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 }
+class DatabaseException extends \Exception{
+    public $path_template;
+    public $real_path;
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
