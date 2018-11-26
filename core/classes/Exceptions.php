f2e5ad095faeb50bb62cea6978aba259393e8a86
Wharley01 <hackerslord96@gmail.com>
2018-11-26 14:30:14 +0100

    added file system exception

--- core/classes/Exceptions.php
+++ core/classes/Exceptions.php
@@ -47,3 +47,15 @@ class DatabaseException extends \Exception{
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 }
+class FileSystemException extends \Exception{
+    public $path_template;
+    public $real_path;
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
