fe25148ce6268cd916499012a220e9951a225700
Wharley01 <hackerslord96@gmail.com>
2018-11-26 14:30:56 +0100

    FileSys refactor, and moveto method added

--- core/classes/FileSys.php
+++ core/classes/FileSys.php
@@ -10,31 +10,35 @@ namespace Path;
 
 
 
-class File
+class FileSys
 {
     private $file_name;
     private $files;
-    private $errors;
+    private $errors         = [];
     private $rules = [
         "retain_name"       => false,
         "accepted_exts"     => [],
         "accepted_types"    => [],
-        "max_size"          => (5 * 1024 * 1024)
         ];//accepted_exts,accepted_types,max_size(bytes),retain_name
-    private $write_folder;
-
+    private $write_folder = "/";
+    public function __construct(){
+        $this->rules["max_size"] = config("FILE->max_size");
+    }
     /**
-     * File constructor.
+     * File file.
      * @param $file
+     * @return $this
      * @throws FileSystemException
      */
-    public function __construct($file)
+    public function file($file)
     {
-        $this->rules["max_size"] = config("FILE->max_zie");
-        if(!@$_FILES[$file])
-            throw new FileSystemException("");
+
         if(is_string($file)){
-            $this->files = is_array($_FILES[$file]['name']) ? $this->restructure($_FILES[$file]) : [$_FILES[$file]];
+            if(isset($_FILES[$file])){
+                $this->files = is_array($_FILES[$file]['name']) ? $this->restructure($_FILES[$file]) : [$_FILES[$file]];
+            }else{
+                $this->files = $file;
+            }
         }elseif(is_array($file)){
             $this->files = is_array($file['name']) ? $this->restructure($file) : [$file];
         }
@@ -44,20 +48,14 @@ class File
 
     /**
      * @param array $rules
-     * @return File
+     * @return FileSys
      */
-    public function setRules(array $rules = []):File
+    public function setRules(array $rules = []):FileSys
     {
         $this->rules = array_merge($this->rules,$rules);
         return $this;
     }
 
-    /**
-     * @param $attr
-     */
-    public function get($attr){
-
-    }
     private function restructure($file){
         $file_ary = array();
         $file_count = count($file['name']);
@@ -72,15 +70,82 @@ class File
         }
         return $file_ary;
     }
+
     /**
      * @param $target_folder
      * @param callable $callback
-     * @return bool
+     * @return FileSys
      */
-    public function moveTo($target_folder,callable $callback):bool
+    public function moveTo($target_folder = null,callable $callback = null)
     {
-        $this->write_folder = $target_folder;
-        return true;
+        $target_folder = $target_folder ?? $this->write_folder;
+//        var_dump($this->files);
+        if(is_array($this->files)) {
+            for ($i = 0; $i < count($this->files); $i++) {
+                $real_file_name  = $this->files[$i]["name"];
+                $file_name       = $this->files[$i]["name"];
+                $file_type       = $this->files[$i]["type"];
+                $file_size       = $this->files[$i]["size"];
+                $file_tmp_name   = $this->files[$i]["tmp_name"];
+                $ext = pathinfo($file_name, PATHINFO_EXTENSION);
+                $file_name = @$this->rules["retain_name"] ? urlencode($file_name) : substr(md5($file_name . (time() + rand(10,50))), 0, 50).".".$ext;
+//            validate extension
+                $this->files[$i]['saved_name']      = $file_name;
+                $this->files[$i]['saved_fullname']  = $target_folder.$file_name;
+
+            if (@$this->rules["accepted_exts"]) {
+                if (!in_array($ext, $this->rules["accepted_exts"])) {
+                    $this->errors[$real_file_name][] = "Invalid Extension {$ext}";
+                }
+            }
+//            check for file type(MiME)
+            if (@$this->rules["accepted_types"]) {
+                if (!in_array($file_type, $this->rules["accepted_types"])) {
+                    $this->errors[$real_file_name][] = "Invalid Extension {$ext}";
+                }
+            }
+//            check if folder exists
+            if(!file_exists($target_folder)){
+                $this->errors[$real_file_name][] = "target folder {$target_folder} does not exist";
+            }
+//            check if file exist
+            if(file_exists($target_folder.$file_name)){
+                $this->errors[$real_file_name][] = "file {$file_name} already exists in  {$target_folder}";
+            }
+//            check if size exceeds the threshold
+            if($file_size > $this->rules["max_size"]){
+                $size_in_mb = $this->rules["max_size"]/(1024*1024);
+                $this->errors[$real_file_name][] = "file size exceeds {$size_in_mb}mb";
+            }
+//            check if there is no error\
+            if(!$this->errors[$real_file_name]){
+                if(!move_uploaded_file($file_tmp_name,$target_folder.$file_name)){
+                    $this->errors[$real_file_name][] = "There was an error while uploading {$real_file_name}";
+                    $this->files[$i]["has_error"] = true;
+
+                }else{
+                    $this->files[$i]["has_error"] = false;
+                }
+            }else{
+                $this->files[$i]["has_error"] = true;
+            }
+                if(is_callable($callback)){
+                    $callback($this->files[$i],$this->errors[$real_file_name]);
+                }else{
+                    if(count($this->files) == 1){
+                         $this->files[$i];
+                    }
+                }
+            }
+
+        }else{
+//            this is file from server not sent from form
+
+        }
+
+
+        /** @var FileSys $this */
+        return $this;
     }
 
     /**
@@ -91,4 +156,24 @@ class File
         return $this->errors;
     }
 
+    /**
+     * @return mixed
+     */
+    public function getFiles(){
+        return $this->files;
+    }
+
+    public function hasError():bool
+    {
+        if($this->errors){
+            foreach ($this->errors as $error){
+                if(count($error) > 0){
+                    return true;
+                }
+            }
+            return false;
+        }else{
+            return false;
+        }
+    }
 }
