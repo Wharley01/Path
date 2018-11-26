901afaa6a57aa7c7d7ccd3eab1cc62ec4d6f7f02
Wharley01 <hackerslord96@gmail.com>
2018-11-26 14:52:10 +0100

    more db test

--- test.php
+++ test.php
@@ -7,18 +7,30 @@ load_class("Database/Models/User.php");
 load_class("FileSys");
 try{
 
-//$user = (new Model\User)->where(['ID' => 5])->select(["Name"])->rightJoin("test2",["test2.col" => "test_table.col"])->as("UserName");
+$user = (new Model\User)
+    ->where(['ID' => 5])
+    ->select("Name")
+    ->rightJoin("test2",["test2.col" => "test_table.col"])
+    ->as("UserName");
 //
-//var_dump((new Model\User)->select("Age")->select($user)->as("Users")->where("ID = 9")->leftJoin("test",["test.col" => "test_table.col"])->get());
+var_dump(
+    (new Model\User)
+        ->select("Age")
+        ->select($user)
+        ->as("Users")
+        ->where("ID = 9")
+        ->leftJoin("test",["test.col" => "test_table.col"])
+        ->get()
+);
 
     var_dump(
         (new Model\User)
             ->select("Name")
-            ->select("MATCH(artist) AGAINST(? IN BOOLEAN MODE)",["\"hello\""])
+            ->select("MATCH(artist) AGAINST(? IN BOOLEAN MODE)",["hello"])
             ->as("artist_relevance")
             ->where(["ID" => 6])
             ->whereCols("Name","Age")
-            ->matches("\"hello\"")
+            ->matches("hello")
             ->batch(0,10)
             ->sortBy("((title_relevance * 3) + artist_relevance)")
             ->get()
