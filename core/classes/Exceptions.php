16306decb0302b95eba4ec8d6df427b07cf98532
Wharley01 <hackerslord96@gmail.com>
2018-12-22 23:20:28 +0100

    Update exceptions to include datastructure exception

--- core/classes/Exceptions.php
+++ core/classes/Exceptions.php
@@ -59,3 +59,17 @@ class FileSystemException extends \Exception{
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 }
+
+class DataStructureException extends \Exception{
+    public $path_template;
+    public $real_path;
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
+
