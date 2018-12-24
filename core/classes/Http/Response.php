22b401f937d6cc83edc09b1210a66e93f0f32a74
Wharley01 <hackerslord96@gmail.com>
2018-12-24 12:55:15 +0100

    enable support for build_path specification for production

--- core/classes/Http/Response.php
+++ core/classes/Http/Response.php
@@ -7,6 +7,8 @@
  */
 
 namespace Path\Http;
+use Path\PathException;
+
 load_class(["Http/MiddleWare"]);
 
 class Response
@@ -14,10 +16,10 @@ class Response
     public $content;
     public $status;
     public $headers = [];
-    public function __construct($content = "",$status = 200)
+    public $build_path = "";
+    public function __construct($build_path = '/')
     {
-        $this->content = $content;
-        $this->status = $status;
+        $this->build_path = $build_path;
         return $this;
     }
     public function json($arr,$status = 200){
@@ -32,10 +34,37 @@ class Response
         $this->headers = ["Content-Type" => "text/plain; charset=UTF-8"];
         return $this;
     }
-    public function html(String $html,$status = 200){
+    public function htmlString(String $html, $status = 200){
         $this->content = $html;
         $this->status = $status;
         $this->headers = ["Content-Type" => "text/html; charset=UTF-8"];
+        return $this;
+    }
+    public function html($file_path,$status = 200){
+//        echo "build_path: ". $this->build_path.PHP_EOL;
+//        echo "file_path: ". $file_path.PHP_EOL;
+        $file = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.$this->build_path."/".$file_path;
+        $public_path = treat_path($this->build_path);
+
+        if(!file_exists($file))
+            throw new PathException(" \"{$file_path}\" does not exist");
+
+//      get the content
+        $file_content = file_get_contents($file);
+
+        $match_resources = preg_replace_callback('/(href|src)=\"?([^">\s]+)\"?[^\s>]*/m',function ($matches) use ($public_path){
+//            var_dump($matches);
+            $resources_path = explode("/",$matches[2]);
+            array_shift($resources_path);
+            $resources_path = join("/",$resources_path);
+
+            return "$matches[1]='{$public_path}{$resources_path}'";
+        },$file_content);
+
+        $this->content = $match_resources;
+        $this->status = $status;
+        $this->headers = ["Content-Type" => "text/html; charset=UTF-8"];
+
         return $this;
     }
     public function stream($data,$status = 200){
@@ -51,7 +80,7 @@ class Response
         $this->headers = array_merge($this->headers,$header);
         return $this;
     }
+    public function file($file_path){
 
-
-
+    }
 }
