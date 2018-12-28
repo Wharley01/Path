5bb96de851acd7dd62a38bdea69ae82ec1c2a672
Wharley01 <hackerslord96@gmail.com>
2018-12-28 09:00:38 +0100

    Added confirm()  and ask() method for user input

--- core/classes/CLI/CLInterface.php
+++ core/classes/CLI/CLInterface.php
@@ -20,5 +20,27 @@ abstract class CLInterface
     ];
     abstract protected function entry(object $argument);
 
+    public function confirm($quest){
+        $handle = fopen ("php://stdin","r");
+        echo $quest."  Y/N:";
+        $input = strtolower(trim(fgets($handle)));
+        if($input != "y" && $input != "n"){
+            echo "Enter Y/N";
+            $this->confirm($quest);
+        }
+        return $input == "y";
+    }
+
+    public function ask($question,$enforce = false){
+        $handle = fopen ("php://stdin","r");
+        echo PHP_EOL.$question;
+        $input = strtolower(trim(fgets($handle)));
+        if($enforce && strlen($input) < 1){
+            $this->ask($question);
+        }
+        return $input;
+    }
+
+
 
 }
