1f397533125a43b1ce79fcfac54705f5ca0ef325
Wharley01 <hackerslord96@gmail.com>
2019-01-03 16:15:02 +0100

    Rename CLI insterface

---
+++ core/classes/CLI/CInterface.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @Author by Sulaiman Adewale.
+ * @Date 12/6/2018
+ * @Time 1:59 AM
+ * @Project Path
+ */
+
+namespace Path\Console;
+
+
+abstract class CInterface
+{
+    protected $name;
+    protected $description;
+    protected $arguments = [
+        "param" => [
+            "desc" => "param description"
+        ]
+    ];
+    abstract protected function entry(object $argument);
+
+    public function confirm($quest){
+        $handle = fopen ("php://stdin","r");
+        echo $quest."  Y/N:";
+        $input = strtolower(trim(fgets($handle)));
+        if($input != "y" && $input != "n"){
+            echo "Enter Y/N";
+            $this->confirm($quest);
+        }
+        return $input == "y";
+    }
+
+    public function ask($question,$enforce = false){
+        $handle = fopen ("php://stdin","r");
+        echo PHP_EOL.$question.":  ";
+        $input = trim(fgets($handle));
+        if($enforce && strlen($input) < 1){
+            $this->ask($question);
+        }
+        return $input;
+    }
+
+}
