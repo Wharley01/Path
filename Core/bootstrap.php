9743a0d0df07ab91e2e6739fa5ba5165e03fec18
Wharley01 <hackerslord96@gmail.com>
2019-01-08 12:53:56 +0100

    Added Boostrap file

---
+++ Core/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+import(
+    "Core/Classes/Database/Connection",
+    "Core/Classes/Utilities",
+    "Core/Classes/Http/Router",
+    "Core/Classes/Http/Response",
+    "Core/Classes/Misc/Validator",
+    "Core/Classes/Cookies",
+    "Core/Classes/Sessions",
+    "Core/Classes/Database/Model",
+    "Core/Classes/Controller",
+    "Core/Classes/Misc/Validator",
+    "Core/Classes/FileSys",
+    "Core/Classes/CLI/CInterface",
+    "Core/Classes/CLI/Console",
+    "Core/Classes/Database/Structure",
+    "Core/Classes/Database/Prototype"
+);
