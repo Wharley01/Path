cb904311e17464864aa7d55878cfb4e3f7ab4a73
Wharley01 <hackerslord96@gmail.com>
2019-01-26 11:50:33 +0100

    Added watcher exception

--- core/classes/Exceptions.php
+++ core/classes/Exceptions.php
@@ -100,3 +100,16 @@ class ConfigException extends \Exception{
     }
 }
 
+class WatcherException extends \Exception{
+    public $path_template;
+    public $real_path;
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+    // custom string representation of object
+    public function __toString() {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
+}
+
