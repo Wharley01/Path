553d8de62e6ded4e3927cfe201ace4ccc82bcd9a
Wharley01 <hackerslord96@gmail.com>
2019-01-26 11:48:13 +0100

    Added caching system

--- core/classes/Cache.php
+++ core/classes/Cache.php
@@ -11,5 +11,36 @@ namespace Path;
 
 class Cache
 {
+    private const CACHE_DIR = "path/.Caches/";
 
+    /**
+     *
+     * @param $key
+     * @return null|string
+     */
+    static public function get($key):?string{
+//        TODO: Do some validation for the key
+        $caches_path = root_path().self::CACHE_DIR.$key.".pch";
+        $value = @file_get_contents($caches_path);
+        return  $value ? $value : null;
+    }
+
+    /**
+     *
+     * @param $key
+     * @param $value
+     * @return bool|int
+     */
+    static public function set($key, $value){
+        $caches_path = root_path().self::CACHE_DIR.$key.".pch";
+        return file_put_contents($caches_path,$value);
+    }
+
+    /**
+     * @param $key
+     */
+    static public function delete($key){
+        $caches_path = root_path().self::CACHE_DIR.$key.".pch";
+        @unlink($caches_path);
+    }
 }
