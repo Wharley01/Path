2baae7df2eeca4b6dc5340ba201b3146f50ef17d
Wharley01 <hackerslord96@gmail.com>
2019-02-06 15:40:27 +0100

    remove unneccary codes

--- core/classes/Test.php
+++ core/classes/Test.php
@@ -11,11 +11,7 @@ namespace Path;
 
 class Test
 {
-    public function __construct()
-    {
-    }
     public static function is_same($arr1,$arr2){
         return trim(json_encode($arr1)) == trim(json_encode($arr2));
     }
-
 }
