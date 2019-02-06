5bb5a5c757c80d6dbf9cb5584810b1bd06c2b66f
Wharley01 <hackerslord96@gmail.com>
2019-02-06 15:40:47 +0100

    refactor storage paths

--- core/classes/Watcher.php
+++ core/classes/Watcher.php
@@ -9,17 +9,18 @@
 namespace Path\Http;
 
 
-use Path\Cache;
+use Path\Storage\Caches;
 use Path\Controller;
 use Path\Controller\Live\TestLive;
 use Path\LiveController;
-use Path\Sessions;
+use Path\Storage\Sessions;
 use Path\WatcherException;
 
 import(
     "core/Classes/Http/Response",
     "core/Classes/LiveController",
-    "core/Classes/Cache"
+    "core/Classes/Storage/Caches",
+    "core/Classes/Storage/Sessions"
     );
 
 class Watcher
@@ -28,29 +29,37 @@ class Watcher
     private $watcher_namespace = "Path\Controller\Live\\";
     private $watchers_path = "Path/Controllers/Live/";
     private $cache = [];
+    public  $socket_key = "";
     private $response = [];
     private $response_instance;
-    public $throw_exception = false;
-    public $error = null;
+    public  $session;
+    private $throw_exception = false;
+    public  $error = null;
+    public  $server;
     private $controller_data = [
-        "root"          => null,
+        "root"                 => null,
         "watchable_methods"    => null,
-        "params"        => null
+        "params"               => null
     ];
-    public $has_changes = false;
+
+    public $has_changes = [];
     /*
      * This will be set to true at first execution
      * so watcher will execute at least once after every initiation
      * */
-    public $has_executed = false;
+    public $has_executed = [];
     /*
      * Holds all controllers being watched
      * */
     private $controller;
-    public function __construct(string $path)
+    /*
+     *
+     * */
+    public function __construct(string $path,$session_id)
     {
         $this->path = trim($path);
         $this->response_instance = new Response();
+        $this->session = new Sessions($session_id);
         $this->extractInfo();
     }
     private function getResources($payload):array {
@@ -73,7 +82,10 @@ class Watcher
             }
         }
 
-        return ['params' => $all_params,'watchable_methods' => $watchable_methods];
+        return [
+            'params' => $all_params,
+            'watchable_methods' => $watchable_methods
+        ];
     }
     private function extractInfo(){
         $path = $this->path;
@@ -88,10 +100,11 @@ class Watcher
         $this->controller_data['params']               = $url_resources['params'];
         $this->controller = $this->getController();
     }
+
     private static function isValidClassName($class_name){
         return preg_match("/^[\w]+$/",$class_name);
     }
-    public function getController():?LiveController{
+    public function getController($message = null):?LiveController{
         $path = $this->controller_data['root'];
         if(!self::isValidClassName($path)){
             $this->throwException("Invalid LiveController Name \"{$path}\" ");
@@ -103,8 +116,9 @@ class Watcher
             $path = $this->watcher_namespace.$path;
 
             $controller = new $path(
-                $this->response_instance,
-                $this->controller_data['params']
+                $this->session,
+                $this->controller_data['params'],
+                $message
             );
 
             return $controller;
@@ -114,8 +128,8 @@ class Watcher
     private function getWatchable(){
 
     }
-    public function watch(){
-        $controller = $this->getController();
+    public function watch($message = null){
+        $controller = $this->getController($message);
         if(!isset($controller->watch_list)){
             $this->throwException("Specify \"watch_list\" in ". get_class($controller));
         }
@@ -131,59 +145,111 @@ class Watcher
     private static function castToString($arr){
         $ret = [];
         foreach ($arr as $key => $value){
-            $ret[$key] = (string) $value;
+            $ret[$key] = is_array($value)?json_encode($value):(string) $value;
         }
         return $ret;
     }
     private static function shouldCache($method,$value){
-        $_value = Cache::get($method);
+        $_value = Caches::get($method);
         return is_null($_value) || $_value !== $value;
     }
 
-
+    private function method($method){
+        return md5($this->socket_key.$method);
+    }
 
     private function cache($watch_list){
         foreach ($watch_list as $method => $value){
+            $method = $this->method($method);
             if(self::shouldCache($method,$value)){
 //                echo "caching {$method} to {$value}".PHP_EOL;
 //                var_dump($value);
-                Cache::set($method,$value);
+                Caches::set($method,$value);
             }
         }
     }
 
-    private  function shouldExecute($method,$value){
-        $cached_value = Cache::get($method);
-        return (is_null($cached_value) || $cached_value != $value) || !$this->has_executed;
+    public function clearCache(){
+        $controller = $this->getController();
+        $watch_list = self::castToString($controller->watch_list);
+
+        foreach ($watch_list as $method => $value) {
+            $method = $this->method($method);
+            Caches::delete($method);
+        }
     }
 
-    public function execute($watch_list,$controller){
+    private  function shouldExecute($method, $value){
+        $_method = $this->method($method);
+        $cached_value = Caches::get($_method);
+        return (is_null($cached_value) || $cached_value != $value) || !@$this->has_executed[$method];
+    }
+
+    private function getPrevValue($method){
+        $_method = $this->method($method);
+        $cached_value = Caches::get($_method);
+        return $cached_value;
+    }
+
+    public function execute($watch_list,$controller,$message = null,$force_execute = false){
         $watchable_methods = $this->controller_data['watchable_methods'];
 //        var_dump($_SESSION);
 
         if(is_null($watchable_methods)){
 //            watch all watchable
+            foreach ($watch_list as $_method => $_value){
+                if($this->shouldExecute($_method,$_value) OR $force_execute){
+                    $this->has_changes[$_method] = true;
+                    $this->has_executed[$_method] = true;
+                    if(!method_exists($controller,$_method)){
+                        $this->response[$_method] = $_value;
+                    }else{
+
+                        $response = is_null($message) ? $controller->{$_method}($this->response_instance,null,$this->session):$controller->{$_method}($this->response_instance,$message,$this->session);
+                        $this->response[$_method] = $response;
+                    }
+                }else{
+                    $this->has_changes[$_method] = false;
+                }
+            }
         }else{
 //            validate the watchlist
             foreach ($watchable_methods as $method){
                 $_method = $method;
-                $_value = $watch_list[$method];
-                if($this->shouldExecute($_method,$_value)){
-                    $this->has_changes = true;
-                    $this->has_executed = true;
-                    if(!method_exists($controller,$_method)){
-                        $this->response[$_method] = $_value;
+                if(isset($watch_list[$method])){
+                    $_value = @$watch_list[$method];
+                    if($this->shouldExecute($_method,$_value) OR $force_execute){
+                        $this->has_changes[$_method] = true;
+                        $this->has_executed[$_method] = true;
+                        if(!method_exists($controller,$_method)){
+                            $this->response[$_method] = $_value;
+                        }else{
+                            $response = is_null($message) ? $controller->{$_method}($this->response_instance,null,$this->session):$controller->{$_method}($this->response_instance,$message,$this->session);
+                            $this->response[$_method] = $response;
+                        }
                     }else{
-                        $response = $controller->{$_method}();
-                        $this->response[$_method] = $response;
-                    }
-                }else{
-                    $this->has_changes = false;
+                        $this->has_changes[$_method] = false;
 
+                    }
                 }
             }
         }
     }
+    public function sendMessage($message){
+        $controller = $this->getController($message);
+        $watch_list = self::castToString($controller->watch_list);
+        $this->execute($watch_list,$controller,$message);
+        $this->cache($watch_list);
+    }
+
+    public function navigate($params,$message = null){
+        $this->controller_data['params'] = $params;
+        $controller = $this->getController($message);
+        $watch_list = self::castToString($controller->watch_list);
+        $this->execute($watch_list,$controller,$message,true);
+        $this->cache($watch_list);
+    }
+
     private function throwException($error_text){
         if($this->throw_exception){
             throw new WatcherException($error_text);
@@ -192,11 +258,55 @@ class Watcher
         }
     }
     public function getResponse(){
-        return $this->response;
+        $response = [];
+        foreach ($this->response as $key => $value){
+//              check if value is an instance of response, then set to appropriate data type
+            if($this->hasChanges($key)){
+                if($value instanceof Response){
+                    $response[$key] = [
+                        "data"           => $value->content,
+                        "status"         => $value->status,
+                        "headers"        => $value->headers,
+                        "has_changes"    => $this->hasChanges($key)
+                    ];
+                }else{
+                    $response[$key] = [
+                        "data"           => $value,
+                        "status"         => 200,
+                        "headers"        => [],
+                        "has_changes"    => $this->hasChanges($key)
+                    ];
+                }
+            }
+
+        }
+        return $response;
     }
     public static function log($text){
         echo PHP_EOL.$text.PHP_EOL;
-        flush();
-        ob_flush();
+//        flush();
+//        ob_flush();
+    }
+
+    /**
+     * @return bool
+     */
+    public function changesOccurred(){
+        foreach ($this->has_changes as $method => $status){
+            if($status === true){
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * @param $method
+     * @return bool
+     */
+    private function hasChanges($method)
+    {
+        return (@$this->has_changes[$method] === true);
     }
 }
