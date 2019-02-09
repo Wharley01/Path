b7394b357871afcfbb953e40333f0865311c7c8a
Wharley01 <hackerslord96@gmail.com>
2019-02-10 00:05:58 +0100

    Added defaukt route for SSE

--- index.php
+++ index.php
@@ -11,7 +11,10 @@ require_once "core/kernel.php";
 require_once "core/bootstrap.php";
 
 try {
+    $__routes = new Router();
+    $__routes->get("SSE/@controller/@action","SSE->watch");
     require_once "path/Routes.php";
 }catch (Throwable $e) {
+    echo "<pre>";
     echo "Path error: " . $e->getMessage() . " trace: <pre>" . $e->getTraceAsString() . "</pre>";
 }
