7f88aa38fff35c7ce16ecf7498074cfa6572d36f
Wharley01 <hackerslord96@gmail.com>
2019-02-16 22:48:40 +0100

    Added default vue config for better quick start

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,38 @@
-
+let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+let config = require("./path/project.pconf.json");
+const PORT = config.PROJECT.port;
+const HOST = config.PROJECT.host;
 module.exports = {
-
+    baseUrl: process.env.NODE_ENV === 'production'
+        ? '/dist/'
+        : '/',
     devServer: {
         proxy: {
             "/api":{
-                target:'http://myvueproj.loc/'
+                target: config.PROJECT.server
+            },
+            "/SSE":{
+                target: config.PROJECT.server
             }
         },
-        port: 3000,
-        host:'localhost',
-        open: true,
+        port: PORT,
+        historyApiFallback: true,
+        host: HOST,
         hot: true,
+        inline: true,
         watchOptions: {
             aggregateTimeout: 300,
             poll: true,
             ignored: /node_modules/
         }
+    },
+    configureWebpack:{
+        plugins:[
+            new BrowserSyncPlugin({
+                host: HOST,
+                port: PORT,
+                proxy: `http://${HOST}:${PORT}`
+            }),
+        ]
     }
-}
+};
