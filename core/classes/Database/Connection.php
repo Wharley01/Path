ae51ec49935b96bbf3b70b7784f0902efe5c2e10
Wharley01 <hackerslord96@gmail.com>
2019-03-17 17:56:37 +0100

    remove commented code

--- core/classes/Database/Connection.php
+++ core/classes/Database/Connection.php
@@ -16,23 +16,6 @@ class Mysql implements DB
 
     public function __construct()
     {
-//        $HOST           = config("DATABASE->host");
-//        $USER           = config("DATABASE->user");
-//        $NAME           = config("DATABASE->name");
-//        $PASSWORD       = config("DATABASE->pass");
-//
-//        try {
-//            $opt = [
-//                \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-//                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-//                \PDO::ATTR_EMULATE_PREPARES   => false,
-//            ];
-//            $this->connection = new \PDO("mysql:host={$HOST};dbname={$NAME}", $USER, $PASSWORD,$opt);
-//            $this->connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-//        } catch (\PDOException $e) {
-//            die($e->getMessage());
-//        }
-//        return $this->connection;
     }
 
     public static function connection(){
