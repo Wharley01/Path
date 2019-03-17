447c25528d04e01a22929bc8066afaa811f28bfe
Wharley01 <hackerslord96@gmail.com>
2019-03-17 17:59:46 +0100

    Stub Migration for test

--- path/Database/Migration/User.php
+++ path/Database/Migration/User.php
@@ -9,7 +9,30 @@
 namespace Path\Database\Migration;
 
 
-class User
-{
+use Path\Database\Model;
+use Path\Database\Structure;
+use Path\Database\Table;
 
+class User implements Table
+{
+    public $table_name = "user";
+    public function install(Structure &$table)
+    {
+        // TODO: Implement install() method.
+    }
+
+    public function uninstall()
+    {
+        // TODO: Implement uninstall() method.
+    }
+
+    public function populate(Model $table)
+    {
+        // TODO: Implement populate() method.
+    }
+
+    public function update(Structure &$table)
+    {
+        // TODO: Implement update() method.
+    }
 }
