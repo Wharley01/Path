78b688338355cb0183ea9fc89636f7bac9d73525
Wharley01 <hackerslord96@gmail.com>
2019-03-21 15:42:52 +0100

    Update DB test Model and classes

---
+++ path/Database/Models/TestM.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* This is automatically generated 
+* Edit to fit your need
+* Powered By: Path
+*/
+
+namespace Path\Database\Models;
+
+
+use Path\Database\Model;
+
+class TestM extends Model
+{
+    protected $table_name               = "checkouts";
+    protected $non_writable_cols        = ["id"];
+    protected $readable_cols            = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
