82b9c207041f51adefb814fd16b963d460788ff5
Wharley01 <hackerslord96@gmail.com>
2019-03-25 23:47:26 +0100

    added external request support for Request

--- core/classes/Http/Request.php
+++ core/classes/Http/Request.php
@@ -16,6 +16,11 @@ class Request
     public $params;
     public $inputs;
     public $fetching;
+    private $headers = [];
+    private $sending_post_feilds = [];
+    private $sending_query_feilds = [];
+    private $IP;
+//    CONST
     public function __construct(){
         $this->METHOD = @$_SERVER["REQUEST_METHOD"];
         $input = file_get_contents("php://input");
@@ -24,7 +29,7 @@ class Request
         }else{
             $input = "[]";
         }
-        if($_SERVER['REQUEST_METHOD']==='POST' && empty($_POST)) {
+        if(@$_SERVER['REQUEST_METHOD']==='POST' && empty($_POST)) {
             $_REQUEST = array_merge($_REQUEST,json_decode($input, true));
         }
 
@@ -53,7 +58,81 @@ class Request
         $this->fetching = $name;
         return $this;
     }
+    public function setRequestIP($IP){
+        $this->headers['REMOTE_ADDR'] = $IP;
+        $this->headers['HTTP_X_FORWARDED_FOR'] = $IP;
+        $this->IP = $IP;
+        return $this;
+    }
 
+    public function setPostFields(array $fields){
+        $this->sending_post_feilds = $fields;
+        return $this;
+    }
 
+    public function setQueryParams(array $fields){
+        $this->sending_query_feilds = $fields;
+        return $this;
+    }
 
+    private function buildRawHeader(array $headers = null){
+        $headers = $headers ?? $this->headers;
+        $header = [];
+        foreach ($headers as $key => $value){
+            $header[] = "{$key}: {$value}";
+        }
+        return $header;
+    }
+
+    public function setHeader(array $headers = []){
+        $this->headers = $headers;
+    }
+
+    private function httpRequest($url,$method):?Response{
+        $ch = curl_init();
+        if($this->sending_query_feilds){
+            $query = http_build_query($this->sending_query_feilds);
+            $url .= "?".$query;
+        }
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,0);
+        if($this->IP){
+            curl_setopt($ch, CURLOPT_PROXY, $this->IP);
+        }
+        if($this->sending_post_feilds){
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $this->sending_post_feilds);
+        }
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt( $ch, CURLOPT_HTTPHEADER,$this->buildRawHeader());
+
+        $headers = [];
+// Then, after your curl_exec call:
+        curl_setopt($ch, CURLOPT_HEADERFUNCTION, function($curl, $header) use (&$headers) {
+            $len = strlen($header);
+            $header = explode(':', $header, 2);
+            if (count($header) >= 2)
+                $headers[strtolower(trim($header[0]))] = trim($header[1]);
+            return $len;
+        });//get headers as array
+
+        $raw_response = curl_exec($ch);
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        $response = new Response();
+        $response->addHeader($headers);
+        $response->status = $status;
+        $response->content = $raw_response;
+
+        return $response;
+    }
+
+    public function get($url):?Response{
+        return $this->httpRequest($url,"GET");
+    }
+
+    public function post($url):?Response{
+        return $this->httpRequest($url,"POST");
+    }
 }
