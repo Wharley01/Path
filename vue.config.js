57d0f713f3f415479eed8698ec89591cc37c9c84
Wharley01 <hackerslord96@gmail.com>
2019-04-08 19:19:48 +0100

    update Vue configuration

--- vue.config.js
+++ vue.config.js
@@ -1,39 +1,51 @@
-let BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+// let BrowserSyncPlugin = require('vue-cli-browser-sync-webpack-plugin');
 let config = require("./path/project.pconf.json");
+const path = require("path");
+
 const PORT = config.PROJECT.port;
 const HOST = config.PROJECT.host;
 module.exports = {
-    baseUrl: process.env.NODE_ENV === 'production'
-        ? '/dist/'
-        : '/',
-    devServer: {
-        proxy: {
-            "/api":{
-                target: config.PROJECT.server
-            },
-            "/SSE":{
-                target: config.PROJECT.server
-            }
-        },
-        port: PORT,
-        historyApiFallback: true,
-        host: HOST,
-        hot: true,
-        inline: true,
-        watchOptions: {
-            aggregateTimeout: 300,
-            poll: true,
-            ignored: /node_modules/
-        }
+  baseUrl: process.env.NODE_ENV === "production" ? "/dist/" : "/",
+  devServer: {
+    proxy: {
+      "/api": {
+        target: config.PROJECT.server
+      },
+      "/SSE": {
+        target: config.PROJECT.server
+      },
+      "/assets": {
+        target: config.PROJECT.server
+      }
     },
-    configureWebpack:{
-        plugins:[
-            new BrowserSyncPlugin({
-                host: HOST,
-                port: PORT,
-                proxy: `http://${HOST}:${PORT}`,
-                reload: false
-            }),
-        ]
+    port: PORT,
+    historyApiFallback: true,
+    host: HOST,
+    hot: true,
+    inline: false,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: true,
+      ignored: /node_modules/
     }
+  },
+  pluginOptions: {
+    browserSync: {
+      host: HOST,
+      port: PORT,
+      proxy: `http://${HOST}:${PORT}`,
+      reload: false,
+      open: true
+    }
+  }
+  // configureWebpack:{
+  //     plugins:[
+  //         new BrowserSyncPlugin({
+  //             host: HOST,
+  //             port: PORT,
+  //             proxy: `http://${HOST}:${PORT}`,
+  //             reload: false
+  //         }),
+  //     ]
+  // }
 };
