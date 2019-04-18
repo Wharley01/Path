904c73a9308baebfe9e24d7d96a5930e07da889b
Wharley01 <hackerslord96@gmail.com>
2019-04-18 09:13:15 +0100

    Mailable Sample

--- path/Mail/Mailables/TestMail.php
+++ path/Mail/Mailables/TestMail.php
@@ -1,12 +1,37 @@
 <?php
-/**
- * @Author by Sulaiman Adewale.
- * @Date 4/18/2019
- * @Time 5:06 AM
- * @Project path
- */
 
-class TestMail
+namespace Path\App\Mail\Mailables;
+
+use Path\Core\Database\Model;
+use Path\Core\Mail\Mailable;
+use Path\Core\Mail\State;
+
+
+class TestMail extends Mailable
 {
+    private $user;
+
+    public $to = [
+        "email" => "Test@wale.com",
+        "name"  => "Adewale"
+    ];
+
+    /**
+     * TestMail constructor.
+     * @param State $state
+     */
+    public function __construct(State $state)
+    {
+    }
+
+    public function title(State $state):String
+    {
+        return "this is the title";
+    }
+
+    public function template(State $state):String
+    {
+        return "Hello {$state->name}";
+    }
 
 }
