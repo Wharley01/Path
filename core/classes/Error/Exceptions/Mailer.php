353221472e826e7f49b75e032a7cbc8540deaf80
Wharley01 <hackerslord96@gmail.com>
2019-04-18 09:10:17 +0100

    Mailer Exception

--- core/classes/Error/Exceptions/Mailer.php
+++ core/classes/Error/Exceptions/Mailer.php
@@ -1,15 +1,27 @@
 <?php
 /**
  * @Author by Sulaiman Adewale.
- * @Date 4/18/2019
- * @Time 6:08 AM
+ * @Date 4/13/2019
+ * @Time 7:47 PM
  * @Project path
  */
 
 namespace Path\Core\Error\Exceptions;
 
 
-class Mailer
-{
+use Throwable;
 
+class Mailer extends \Exception
+{
+    public $path_template;
+    public $real_path;
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+    // custom string representation of object
+    public function __toString()
+    {
+        return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
+    }
 }
