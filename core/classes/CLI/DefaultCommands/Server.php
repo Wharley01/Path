80705feca0a473eedcb425b6d26aa67961b3f059
Wharley01 <hackerslord96@gmail.com>
2019-10-04 11:06:16 +0100

    improved local server experience

--- core/classes/CLI/DefaultCommands/Server.php
+++ core/classes/CLI/DefaultCommands/Server.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @Author by Sulaiman Adewale.
  * @Date 12/24/2018
@@ -12,6 +13,7 @@ namespace Path\Core\CLI\DefaultCommands;
 
 use Path\Core\CLI\CInterface;
 use Path\Core\CLI\Console;
+use Path\Plugins\PHPMailer\Exception;
 
 class Server extends CInterface
 {
@@ -36,14 +38,40 @@ class Server extends CInterface
 
     public function entry($argument)
     {
-        $argument = (object)$argument;
+        $argument = (object) $argument;
         $port = $this->getPort(@$argument->port);
-        $cmd = "php -S localhost:{$port} index.php";
+        $cmd = "cd " . ROOT_PATH . "/ && php -S localhost:{$port}";
         echo PHP_EOL;
-        $this->write("`green`[+] Server started at: `green` localhost:{$port}".PHP_EOL);
-        $this->write(PHP_EOL.'`blue`Press ^C to terminate`blue`'.PHP_EOL);
+        $this->write("`green`[+] Server started at: `green` localhost:{$port}" . PHP_EOL);
+        $this->write(PHP_EOL . '`blue`Press ^C to terminate`blue`' . PHP_EOL);
+        var_dump(PHP_OS);
 
-        $proc = popen("$cmd 2>&1 ; echo Exit status : $?", 'r');
+        if (mb_strpos(PHP_OS, 'win') !== false) {
+            try {
+                shell_exec("start http://localhost:{$port}");
+            } catch (Exception $e) {
+                // 
+            }
+        } elseif (mb_strpos(PHP_OS, 'mac')) {
+            try {
+                shell_exec("open http://localhost:{$port}");
+            } catch (Exception $e) {
+                // 
+            }
+        } elseif (mb_strpos(PHP_OS, 'linux')) {
+            try {
+                shell_exec("xdg-open http://localhost:{$port}");
+            } catch (Exception $e) {
+                // 
+            }
+        } else {
+            try {
+                shell_exec("chrome http://localhost:{$port}");
+            } catch (Exception $e) {
+                // 
+            }
+        }
 
+        $proc = shell_exec($cmd);
     }
 }
