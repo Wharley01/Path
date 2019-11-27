49efb064fa3c4e4822431050f91fbae846e0c18e
Wharley01 <hackerslord96@gmail.com>
2019-11-27 03:33:39 +0100

    added support for custom IP when starting server

--- core/classes/CLI/DefaultCommands/Server.php
+++ core/classes/CLI/DefaultCommands/Server.php
@@ -18,6 +18,7 @@ use Path\Plugins\PHPMailer\Exception;
 class Server extends CInterface
 {
     private const DEFAULT_PORT = 8080;
+    private const DEFAULT_IP = "localhost";
     public $name = "server";
 
     public $description = "start development server";
@@ -28,47 +29,70 @@ class Server extends CInterface
         ],
         "port"  => [
             "desc" => "port to use"
+        ],
+        "--ip=" => [
+            "desc" => "IP to use"
         ]
     ];
 
     private function getPort($port)
     {
-        return $port ?? self::DEFAULT_PORT;
+        if (!$port) {
+            return self::DEFAULT_PORT;
+        } elseif (@filter_var(
+            $port,
+            FILTER_VALIDATE_REGEXP,
+            ["options" => ["regexp" => "/^([\d]{1,5})$/"]]
+        )) {
+            return $port;
+        } else {
+            $this->write("\n`light_red` PORT `light_red` `white`{$port}`white` `light_red`is invalid `light_red`\n");
+            throw new Exception("Error occured");
+        }
+    }
+
+    private function getIp($ip)
+    {
+        if (@filter_var($ip, FILTER_VALIDATE_IP)) {
+            return $ip;
+        } else {
+            $this->write("\n`light_red` IP`light_red` `white`{$ip}`white` `light_red`is invalid `light_red`\n");
+            throw new Exception("Error occured");
+        }
     }
 
     public function entry($argument)
     {
-        $argument = (object) $argument;
-        $port = $this->getPort(@$argument->port);
-        $cmd = "cd " . ROOT_PATH . "/ && php -S localhost:{$port}";
+        $port = $this->getPort(@$argument['port']);
+        $ip = $this->getIp($argument['--ip=']);
+        $cmd = "cd " . ROOT_PATH . "/ && php -S {$ip}:{$port}";
         echo PHP_EOL;
-        $this->write("`green`[+] Server started at: `green` localhost:{$port}" . PHP_EOL);
+        $this->write("`green`[+] Server started at: `green` {$ip}:{$port}" . PHP_EOL);
         $this->write(PHP_EOL . '`blue`Press ^C to terminate`blue`' . PHP_EOL);
-        var_dump(PHP_OS);
 
         if (mb_strpos(PHP_OS, 'win') !== false) {
             try {
-                shell_exec("start http://localhost:{$port}");
+                shell_exec("start http://{$ip}:{$port}");
             } catch (Exception $e) {
-                // 
+                //
             }
         } elseif (mb_strpos(PHP_OS, 'mac')) {
             try {
-                shell_exec("open http://localhost:{$port}");
+                shell_exec("open http://{$ip}:{$port}");
             } catch (Exception $e) {
-                // 
+                //
             }
         } elseif (mb_strpos(PHP_OS, 'linux')) {
             try {
-                shell_exec("xdg-open http://localhost:{$port}");
+                shell_exec("xdg-open http://{$ip}:{$port}");
             } catch (Exception $e) {
-                // 
+                //
             }
         } else {
             try {
-                shell_exec("chrome http://localhost:{$port}");
+                shell_exec("chrome http://{$ip}:{$port}");
             } catch (Exception $e) {
-                // 
+                //
             }
         }
 
