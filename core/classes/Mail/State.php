c4e3b225f9ea7cc1de527e1db25515f44fc72186
Wharley01 <hackerslord96@gmail.com>
2020-03-27 04:46:07 +0100

    improvement

--- core/classes/Mail/State.php
+++ core/classes/Mail/State.php
@@ -12,5 +12,16 @@ namespace Path\Core\Mail;
 
 class State
 {
+    private array $state = [];
 
+    public function bind(array $state_array){
+        $this->state = array_merge($this->state, $state_array);
+        foreach ($this->state as $key=>$value){
+            $this->{$key} = $value;
+        }
+    }
+
+    public function allState(){
+        return $this->state;
+    }
 }
