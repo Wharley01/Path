d6dba359d17be2b49bc0a6c2c59a23bea73650c7
wharley01 <hackerslord96@gmail.com>
2020-11-02 17:15:26 +0100

    Clean-up

--- path/Routes.php
+++ path/Routes.php
@@ -8,11 +8,9 @@ use Path\Core\Http\Response;
 use Path\Core\Http\Router;
 
 $router = new Router();
-//$router->response->disableCORS('http://192.168.0.128:9001');
-$router->response->disableCORS(config("PROJECT->UI_URI"));
-$router->group("v1", function (Router $router){
-    $router->post("/handle-incoming-message", WhatsAppWebHook::class);
-});
+//Enter your UI SERVERS Seperated by comma
+$router->response->disableCORS("");
+
 $router->get("SSE/@controller/@action", 'Path\Plugins\SSEController\SSEServer->watch');
 $router->error404(function (Request $request, Response $response) {
     return $response->error('Error 404', [], 404);
@@ -21,10 +19,8 @@ $router->graph('/path-graph');
 $router->setBuildPath("/");
 
 $router->get('/', function (Request $request,Response $response) {
-    return $response->success('BuySafe.ng API v1');
+    return $response->success('PATH v2.0.3');
 });
-$router->post("/seller/deauthorize", WebHook::class.'->deAuthorize');
-$router->post('/payment-hook', HookService::class.'->raveHook');
 
 $router->end();
 
